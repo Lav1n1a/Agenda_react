f63c7117be749229967cc48d9c3b84330cc6b56a
Lavinia <lavinia.camk01@gmail.com>
2024-02-04 09:46:39 -0400

    Segundo commit

--- webserver/src/controllers/createUserController.ts
+++ webserver/src/controllers/createUserController.ts
@@ -2,15 +2,23 @@ import { Request, Response } from 'express'
 import { prisma } from '../database/prismaClient' 
 
  export class CreateUser {
-    handle(req: Request, res: Response) {
-        const { email, senha } = req.body
+    async handle(req: Request, res: Response) {
+        try{
+            const { email, senha } = req.body
 
-        const user = prisma.user.create({
+        const user = await prisma.user.create({
             data: {
                 email, 
                 senha
             }
         })
         return res.json(user)
+
+        }catch{
+            (err: any) => {
+                console.log(err)
+                res.status(500)
+            }
+        }
     }
 }
--- webserver/src/index.ts
+++ webserver/src/index.ts
@@ -1,13 +1,14 @@
 import express, { Application } from 'express';
-import router from './routes/getRouter';
+import showUsers from './routes/getRouter';
 import createUserRouter from './routes/createUserRouter'
 
 const app: Application = express();
 app.use(express.json())
-app.use(router)
+
 
 //Rotas
 app.use(createUserRouter);
+app.use(showUsers)
 
 app.listen(4007, () => console.log('Rodando na porta 4007'))
 
